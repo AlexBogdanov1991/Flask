f6f836bce26c34caed7189a022cacd9c5da64f97
BogdanovALTX <sir.bogdanov2015@yandex.ru>
2025-07-05 12:39:27 +0300

    new

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route('/')
 def hello():
-    return "Hello, Flask!"
+    return "Hello, world!"
+@app.route('/info')
+def info():
+    return "This is an informational page."
+@app.route('/reverse/<text>')
+def reverse(text):
+    return text[::-1]
+
+@app.route("/calc/<int:a>/<int:b>")
+def calc(a, b):
+    result = a + b
+    return f"The sum of {a} and {b} is {result}."
+
+@app.route('/user/<string:name>/<int:age>')
+def user(name, age):
+    return f"Hello, {name}. You are {age} years old."
+
 if __name__ == "__main__":
     app.run(debug=True)
