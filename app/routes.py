7dd9b34514566c6581b2b3f8ca0f2fcbd423812a
BogdanovALTX <sir.bogdanov2015@yandex.ru>
2025-07-09 11:02:59 +0300

    new

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,15 @@
+from flask import render_template
 from app import app
 
-@app.route("/hello")
-def hello():
-    return "Hello, world!"
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-@app.route("/info")
-def info():
-    return "This is an informational page."
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/about")
+def contact():
+    return render_template("contact.html")
+
